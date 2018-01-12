7da3f41d5eed2b21bc342ea805a20a247c0a9109
Phonic Mouse <phonicmouse@gmail.com>
2018-01-12 02:13:01 +0000

    added basic config file

--- routes/main.js
+++ routes/main.js
@@ -1,12 +1,15 @@
 var express = require('express');
-var router = express.Router();
-
+var randomstring = require("randomstring");
 var db = require('../db');
 
-/* GET home page. */
+var router = express.Router();
+
+var config = require('../config');
+
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'Linker'
+    title: config.title,
+    slogan: config.slogan
   });
 });
 
@@ -14,7 +17,24 @@ router.post('/shorten', function(req, res, next) {
   try {
     var json = JSON.parse(req.body);
     var link = json.link;
-    
+    var isUnique = false;
+    while (isUnique == false) {
+      var id = randomstring.generate({
+        length: config.linker.length,
+        charset: config.linker.charset
+      });
+      db.get(id, function(err, value) {
+        if (err) {
+          if (err.notFound) {
+            isUnique = true;
+          }
+          // I/O or other error, pass it up the callback chain
+          return callback(err)
+        }
+
+        // .. handle `value` here
+      });
+    }
   } catch (err) {
     // return error json
   }
