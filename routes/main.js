3f9f47b6509fe9e9c35319dc7f6c1ca4fafa46e5
Phonic Mouse <phonicmouse@gmail.com>
2018-01-14 22:27:06 +0000

    shorten works

--- routes/main.js
+++ routes/main.js
@@ -13,37 +13,17 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/shorten', function(req, res, next) {
-  try {
-    // var json = JSON.parse(req.body);
-    var json = req.body;
-    var url = json.url;
-    var isUnique = false;
-    while (isUnique == false) {
-      var id = randomstring.generate({
-        length: config.linker.length,
-        charset: config.linker.charset
-      });
-      db.get(id, function(err, value) {
-        if (err.notFound) {
-          isUnique = true;
-          db.put(id, {
-            url: url
-          }, function(err){
-            res.send({
-              error: false,
-              id: id
-            });
-          });
-        }
-      });
-    }
-  } catch (err) {
+  var url = req.body.url;
+  var id = randomstring.generate({
+    length: config.linker.length,
+    charset: config.linker.charset
+  });
+  db.put(id, { url: url}, function(err) {
+    if (err) console.log('Whoops!', err);
     res.send({
-      error: true,
-      message: 'this is a TODO error',
-      rawobj: err
+      id: id
     });
-  }
+  });
 });
 
 module.exports = router;
